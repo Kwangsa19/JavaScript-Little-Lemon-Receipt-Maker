d51563167c8c779a02f98e31e2f988998dbb8e7a
Ketmanto Wangsa <135963482+Kwangsa19@users.noreply.github.com>
2024-01-02 19:23:13 +1100

    Update Final-Project.js

--- Final-Project.js
+++ Final-Project.js
@@ -1,4 +1,6 @@
-// Given variables
+// Variables for this task are Italian pasta, rice with veggies, chicken with potatoes, and vegetarian pizza.  
+
+// const is immutable. Once assigned, it stays that way.
 const dishData = [
     {
         name: "Italian pasta",
@@ -17,12 +19,16 @@ const dishData = [
         price: 6.45
     },
 ]
+
+// const is immutable. Once assigned, it stays that way.
 const tax = 1.20;
 
 // Implement getPrices()
+// function is a reusable code that executes a certain task. 
+// TaxBoolean is required in this task as a value, while the function name is getPrices. 
 function getPrices(taxBoolean) {
     
-    for (object of dishData){
+    for (object of dishData){ // every object in the dishData
         let finalPrice;
 
         if(taxBoolean === true){
@@ -34,29 +40,31 @@ function getPrices(taxBoolean) {
             return;
         }
         
-        console.log(`Dish: ${object['name']} Price: $${finalPrice}`);
+        console.log(`Dish: ${object['name']} Price: $${finalPrice}`); //print the object, price and its respective final price
     }
 
 }
 
-// Implement getDiscount()
+// Implement getDiscount() function
+// Valuesor arguments : taxBoolean, guests
 function getDiscount( taxBoolean, guests) {
     getPrices(taxBoolean);
 
-    if(typeof guests === "number" && guests > 0 && guests < 30){
+    if(typeof guests === "number" && guests > 0 && guests < 30){ // if it is a number, the value is true, otherwise false. Second, it will check the guests and it is greater than 0 and less than 30. 
+        // if all three conditions above are fulfilled, the overall condition will be true and code be executed. If the overall condition is false, the code will be skipped. 
         let discount = 0;
 
-        if(guests < 5){
+        if(guests < 5){ //check how many guests they have
             discount = 5;
         } else if(guests >= 5){
             discount = 10;
         }
         console.log('Discount is: $' + discount);
-    } else {
+    } else { 
             console.log('The second argument must be a number between 0 and 30');
     }
 }
 
-// Call getDiscount()
+// Call getDiscount() functions
 getDiscount(true, 2)
 getDiscount(false, 10)
